4acd9165cfb58e1cb01fa8f01f7051eabe700051
Sachin Goyal <sachin@fanduel.com>
2019-08-11 19:36:57 +0100

    Kill all and then recreate images, oh man it's silly of Docker.

--- rebuildAndPush.sh
+++ rebuildAndPush.sh
@@ -1,10 +1,5 @@
-export SERVICE=websockets
-export VERSION=1.0
-
-docker rmi $(docker images -qa -f 'dangling=true')
+docker rmi $(docker images -qa 'sachingoyaldocker/baat-org-websockets')
 
 ./gradlew clean build bootJar
-
-docker build --no-cache -t sachingoyaldocker/baat-org-${SERVICE}:${VERSION} . 
-
-docker push sachingoyaldocker/baat-org-${SERVICE}:${VERSION}
+docker build --no-cache -t sachingoyaldocker/baat-org-websockets:1.0 . 
+docker push sachingoyaldocker/baat-org-websockets:1.0
