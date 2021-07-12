d5528a5881bae2b916c576e01f20bac49cdfb70e
Sachin Goyal <sachin@fanduel.com>
2021-07-12 23:37:55 +0100

    Smarter scripts

--- redeployAll.sh
+++ redeployAll.sh
@@ -1,3 +1,3 @@
-kubectl create namespace baat
+kubectl create namespace baat --dry-run=client -o yaml | kubectl apply -f -
 kubectl delete -f infrastructure/k8s/service-deployment.yml --namespace=baat
 kubectl create -f infrastructure/k8s/service-deployment.yml --namespace=baat
